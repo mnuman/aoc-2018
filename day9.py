e6a30a64ae75e5a0d60b70bcbebbf55ed06c68ef
Milco Numan <milco.numan@gmail.com>
2018-12-12 20:08:56 +0100

    TODO: Implement special case and score

--- day9.py
+++ day9.py
@@ -1,40 +1,52 @@
-You talk to the Elves while you wait for your navigation system to initialize. To pass the time, they introduce you to their favorite marble game.
+"""
+You talk to the Elves while you wait for your navigation system to initialize. T
+o pass the time, they introduce you to their favorite marble game.
 
-The Elves play this game by taking turns arranging the marbles in a circle according to very particular rules. The marbles are numbered starting with 0 and increasing by 1 until every marble has a number.
+The Elves play this game by taking turns arranging the marbles in a circle according to very particular rules.
+The marbles are numbered starting with 0 and increasing by 1 until every marble has a number.
 
-First, the marble numbered 0 is placed in the circle. At this point, while it contains only a single marble, it is still a circle: the marble is both clockwise from itself and counter-clockwise from itself. This marble is designated the current marble.
+First, the marble numbered 0 is placed in the circle. At this point, while it contains only a single marble,
+it is still a circle: the marble is both clockwise from itself and counter-clockwise from itself.
+ This marble is designated the current marble.
 
-Then, each Elf takes a turn placing the lowest-numbered remaining marble into the circle between the marbles that are 1 and 2 marbles clockwise of the current marble. (When the circle is large enough, this means that there is one marble between the marble that was just placed and the current marble.) The marble that was just placed then becomes the current marble.
+Then, each Elf takes a turn placing the lowest-numbered remaining marble into the circle between
+the marbles that are 1 and 2 marbles clockwise of the current marble. (When the circle is
+large enough, this means that there is one marble between the marble that was just placed
+and the current marble.) The marble that was just placed then becomes the current marble.
 
-However, if the marble that is about to be placed has a number which is a multiple of 23, something entirely different happens. First, the current player keeps the marble they would have placed, adding it to their score. In addition, the marble 7 marbles counter-clockwise from the current marble is removed from the circle and also added to the current player's score. The marble located immediately clockwise of the marble that was removed becomes the new current marble.
+However, if the marble that is about to be placed has a number which is a multiple of 23,
+something entirely different happens. First, the current player keeps the marble they would
+have placed, adding it to their score. In addition, the marble 7 marbles counter-clockwise
+from the current marble is removed from the circle and also added to the current player's score.
+The marble located immediately clockwise of the marble that was removed becomes the new current marble.
 
 For example, suppose there are 9 players. After the marble with value 0 is placed in the middle, each player (shown in square brackets) takes a turn. The result of each of those turns would produce circles of marbles like this, where clockwise is to the right and the resulting current marble is in parentheses:
 
-[-] (0)
-[1]  0 (1)
-[2]  0 (2) 1 
-[3]  0  2  1 (3)
-[4]  0 (4) 2  1  3 
-[5]  0  4  2 (5) 1  3 
-[6]  0  4  2  5  1 (6) 3 
+[-] (0)                         current_pos = 0
+[1]  0 (1)                      len=1, insert_pos = 1, current_pos <-- 1
+[2]  0 (2) 1                    len=2, insert_pos = 1, current_pos <-- 1
+[3]  0  2  1 (3)                len=3, insert_pos = 4, current_pos <-- 4
+[4]  0 (4) 2  1  3              len=4, insert_pos = 1
+[5]  0  4  2 (5) 1  3
+[6]  0  4  2  5  1 (6) 3
 [7]  0  4  2  5  1  6  3 (7)
-[8]  0 (8) 4  2  5  1  6  3  7 
-[9]  0  8  4 (9) 2  5  1  6  3  7 
-[1]  0  8  4  9  2(10) 5  1  6  3  7 
-[2]  0  8  4  9  2 10  5(11) 1  6  3  7 
-[3]  0  8  4  9  2 10  5 11  1(12) 6  3  7 
-[4]  0  8  4  9  2 10  5 11  1 12  6(13) 3  7 
-[5]  0  8  4  9  2 10  5 11  1 12  6 13  3(14) 7 
+[8]  0 (8) 4  2  5  1  6  3  7
+[9]  0  8  4 (9) 2  5  1  6  3  7
+[1]  0  8  4  9  2(10) 5  1  6  3  7
+[2]  0  8  4  9  2 10  5(11) 1  6  3  7
+[3]  0  8  4  9  2 10  5 11  1(12) 6  3  7
+[4]  0  8  4  9  2 10  5 11  1 12  6(13) 3  7
+[5]  0  8  4  9  2 10  5 11  1 12  6 13  3(14) 7
 [6]  0  8  4  9  2 10  5 11  1 12  6 13  3 14  7(15)
-[7]  0(16) 8  4  9  2 10  5 11  1 12  6 13  3 14  7 15 
-[8]  0 16  8(17) 4  9  2 10  5 11  1 12  6 13  3 14  7 15 
-[9]  0 16  8 17  4(18) 9  2 10  5 11  1 12  6 13  3 14  7 15 
-[1]  0 16  8 17  4 18  9(19) 2 10  5 11  1 12  6 13  3 14  7 15 
-[2]  0 16  8 17  4 18  9 19  2(20)10  5 11  1 12  6 13  3 14  7 15 
-[3]  0 16  8 17  4 18  9 19  2 20 10(21) 5 11  1 12  6 13  3 14  7 15 
-[4]  0 16  8 17  4 18  9 19  2 20 10 21  5(22)11  1 12  6 13  3 14  7 15 
-[5]  0 16  8 17  4 18(19) 2 20 10 21  5 22 11  1 12  6 13  3 14  7 15 
-[6]  0 16  8 17  4 18 19  2(24)20 10 21  5 22 11  1 12  6 13  3 14  7 15 
+[7]  0(16) 8  4  9  2 10  5 11  1 12  6 13  3 14  7 15
+[8]  0 16  8(17) 4  9  2 10  5 11  1 12  6 13  3 14  7 15
+[9]  0 16  8 17  4(18) 9  2 10  5 11  1 12  6 13  3 14  7 15
+[1]  0 16  8 17  4 18  9(19) 2 10  5 11  1 12  6 13  3 14  7 15
+[2]  0 16  8 17  4 18  9 19  2(20)10  5 11  1 12  6 13  3 14  7 15
+[3]  0 16  8 17  4 18  9 19  2 20 10(21) 5 11  1 12  6 13  3 14  7 15
+[4]  0 16  8 17  4 18  9 19  2 20 10 21  5(22)11  1 12  6 13  3 14  7 15
+[5]  0 16  8 17  4 18(19) 2 20 10 21  5 22 11  1 12  6 13  3 14  7 15
+[6]  0 16  8 17  4 18 19  2(24)20 10 21  5 22 11  1 12  6 13  3 14  7 15
 [7]  0 16  8 17  4 18 19  2 24 20(25)10 21  5 22 11  1 12  6 13  3 14  7 15
 The goal is to be the player with the highest score after the last marble is used up. Assuming the example above ends after the marble numbered 25, the winning score is 23+9=32 (because player 5 kept marble 23 and removed marble 9, while no other player got any points in this very short example game).
 
@@ -47,3 +59,41 @@ Here are a few more examples:
 30 players; last marble is worth 5807 points: high score is 37305
 What is the winning Elf's score?
 """
+
+
+class Game:
+    """Represent the Elves'  marble game. Counterclockwise means shift to right
+       in the list for this implementation!
+    """
+
+    def __init__(self, number_of_players, last_marble):
+        self.position = 0
+        self.number_of_players = number_of_players
+        self.sequence = [0]
+        self.players = [0] * number_of_players
+        self.player = 0
+        self.last_marble = last_marble
+        self.current_marble = 1
+
+    def insert_pos(self):
+        return (self.position + 1) % len(self.sequence) + 1
+
+    def play(self):
+        if self.current_marble % 23 != 0:
+            i = self.insert_pos()
+            self.sequence.insert(i, self.current_marble)
+            self.position = i
+        else:
+            pass  # special logic
+        self.player = (self.player + 1) % self.number_of_players
+        self.current_marble += 1
+
+
+def day9():
+    g = Game(9, 10)
+    for i in range(11):
+        g.play()
+        print(g.sequence)
+
+
+day9()
