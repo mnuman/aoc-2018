7f7bb7d0e72ed1c7c02540923fa21a96b2bd934e
Milco Numan <milco.numan@gmail.com>
2018-12-27 14:15:48 +0100

    Solved day14-2

--- day14.py
+++ day14.py
@@ -25,5 +25,39 @@ def day14(number_of_recipes):
                     for c in recipes.recipes[number_of_recipes:number_of_recipes+10]])
 
 
+def day14_2(sublist):
+    recipes = RecipeList([3, 7])
+    foundIt = False
+    total = 0
+    while not foundIt:
+        recipes.cycle()
+        total += 1
+        foundIt = (recipes.recipes[-1] == sublist[-1] and
+                   recipes.recipes[-2] == sublist[-2] and
+                   recipes.recipes[-3] == sublist[-3] and
+                   recipes.recipes[-4] == sublist[-4] and
+                   recipes.recipes[-5] == sublist[-5] and
+                   recipes.recipes[-6] == sublist[-6]
+                   )
+        if foundIt:
+            print(recipes.recipes[-10:])
+            return len(recipes.recipes) - 6
+        else:
+            foundIt = (recipes.recipes[-2] == sublist[-1] and
+                       recipes.recipes[-3] == sublist[-2] and
+                       recipes.recipes[-4] == sublist[-3] and
+                       recipes.recipes[-5] == sublist[-4] and
+                       recipes.recipes[-6] == sublist[-5] and
+                       recipes.recipes[-7] == sublist[-6]
+                       )
+            if foundIt:
+                print(recipes.recipes[-10:])
+                return len(recipes.recipes) - 7
+        if total % 5000 == 0:
+            print(total)
+
+
 # print(day14(890691))
 # 8176111038
+print(day14_2(sublist=[8, 9, 0, 6, 9, 1]))
+# 20225578
--- day14_test.py
+++ day14_test.py
@@ -46,3 +46,28 @@ def test_day14_example_eightteen_recipes():
 
 def test_day14_example_2018_recipes():
     day14.day14(2018) == '5941429882'
+
+
+def test_day14_51589():
+    idx = day14.day14_2('51589', interval=20)
+    assert idx == 9
+
+
+def test_day14_01245():
+    idx = day14.day14_2('01245', interval=10)
+    assert idx == 5
+
+
+def test_day14_92510():
+    idx = day14.day14_2('92510', interval=50)
+    assert idx == 18
+
+
+def test_day14_59414():
+    idx = day14.day14_2('59414', interval=50)
+    assert idx == 2018
+
+# 51589 first appears after 9 recipes.
+# 01245 first appears after 5 recipes.
+# 92510 first appears after 18 recipes.
+# 59414 first appears after 2018 recipes.
